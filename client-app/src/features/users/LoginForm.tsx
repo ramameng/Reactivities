8f01a958e19dfcc2c7e60790feedbd1fcac87ce9
ramayasa <ramayasamade@gmail.com>
2021-03-19 17:09:42 +0800

    End of section 13

---
+++ client-app/src/features/users/LoginForm.tsx
@@ -0,0 +1,24 @@
+import { Formik } from 'formik';
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import MyTextInput from '../../app/common/form/MyTextInput';
+import { useStore } from '../../app/stores/store';
+
+export default observer(function LoginForm() {
+    const {userStore} = useStore();
+
+    return (
+        <Formik 
+            initialValues={{email: '', password: ''}} 
+            onSubmit={values => userStore.login(values)}>
+            {({handleSubmit, isSubmitting}) => (
+                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                    <MyTextInput name='email' placeholder='Email' />
+                    <MyTextInput name='password' placeholder='Passowrd' type='password' />
+                    <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
+                </Form>
+            )}
+        </Formik>
+    )
+})
